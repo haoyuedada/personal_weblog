d2470f9f4b912d6f0e2a0a8db4e67fc4066cfaeb
chenhaoyue <2153918516@qq.com>
2021-02-26 18:11:31 +0800

    首页调整

--- blog/src/main.js
+++ blog/src/main.js
@@ -6,12 +6,16 @@
  */
 import Vue from 'vue'
 import App from './App.vue'
+//video
 import Video from 'video.js'
 import 'video.js/dist/video-js.css'
+//element-ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 Vue.prototype.$video = Video
-
+Vue.use(ElementUI)
 import router from './router'
 
 new Vue({
--- blog/src/views/login/index.vue
+++ blog/src/views/login/index.vue
@@ -7,22 +7,21 @@
 <template>
   <div id="mainPage">
     <!-- <img :src="login_img"> -->
-    <!-- muted 控制进入页面就播放，-->
+    <!-- muted 控制进入页面就播放-->
     <video id="loginVideo" autoplay=“autoplay” loop="loop" muted="muted" src="@/assets/video/Crowdrise_2.mp4">your browser does not support the video tag</video>
     <div class="userInput">
-      <div>Email address</div>
+      <div class="input_title">Email address</div>
       <div>
-        <input type="text">
+        <input type="text" placeholder="请输入用户名">
       </div>
-      <div>Set passward</div>
+      <div class="input_title">Set passward</div>
       <div>
-        <input type="text">
+        <input type="text" placeholder="请输入密码">
       </div>
       <div>
-        <el-radio v-model="radio" label="label"></el-radio>
-        <span>Show password</span>
+        <el-checkbox class="checkbox" v-model="checked" label="Show password"></el-checkbox>
       </div>
-      <div>Sign Up -></div>
+      <div class="login_button">Sign Up -></div>
     </div>
   </div>
 </template>
@@ -34,6 +33,7 @@
     name:'login',
     data(){
       return {
+        checked:true
         //login_img
       }
     }
@@ -50,11 +50,42 @@
   position: relative;
   width:600px;
   height: 600px;
-  left: 73px;
+  left: 178px;
   top: 30px;
 }
 .userInput{
   display: inline-block;
   position: relative;
+  top: -185px;
+  left: 142px;
+  .input_title{
+    font-weight: bold;
+    font-size: 13px;
+    margin-bottom: 10px;
+    margin-top: 10px;
+  }
+  input{
+    border-radius: 6px;
+    border: 1px solid #C0B8BB;
+    height: 30px;
+    width: 300px;
+    font-size: 12px;
+    padding: 2px 4px 2px 4px;
+  }
+  .el-checkbox__label{
+    font-size: 10px;
+  }
+  .login_button{
+    width:300px;
+    height: 40px;
+    border-radius: 7px;
+    text-align: center;
+    line-height: 40px;
+    position: relative;
+    top: 15px;
+    background-color: #1AAD85;
+    color: white;
+    font-size: 14px;
+  }
 }
 </style>
